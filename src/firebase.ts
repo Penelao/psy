dfd1e62548867cf16fdccc889669a47672527518
diego <diegodraka@hotmail.com>
2023-10-08 15:27:07 +0200

    Fix database connection

--- src/firebase.ts
+++ src/firebase.ts
@@ -19,4 +19,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-connectFirestoreEmulator(db, '127.0.0.1', 8080);
+import.meta.env.DEV && connectFirestoreEmulator(db, '127.0.0.1', 8080);
